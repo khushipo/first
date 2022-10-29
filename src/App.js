32b2c21ba7e074278d69072a3f3167f5e580b837
khushipo <dheerajpoddar6@gmail.com>
2022-10-29 23:10:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>jai mata dii</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>jai mata rani</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
